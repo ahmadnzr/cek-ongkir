5cc4a8fad88fd0b11d6348d6174b8665d318552c
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2024-08-31 02:05:27 +0700

    integrate with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
 import styled from "styled-components";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
 
 import { Text } from "./components";
-import { Button, Select } from "antd";
-import { Colors } from "./helpers/utils";
+import { Button, InputNumber, Select } from "antd";
+import { Colors, formatRupiah } from "./helpers/utils";
+import { useFecthCost, useFetchCity, useFetchProvince } from "./helpers/hooks";
+import { useState } from "react";
+import { Courier } from "./helpers/types/responseApi";
+
+type Inputs = {
+  fromProvince: string;
+  fromCity: string;
+  toProvince: string;
+  toCity: string;
+  weight: string;
+  courier: "jne" | "pos" | "tiki";
+};
 
 function App() {
+  const { handleSubmit, watch, control } = useForm<Inputs>();
+
+  const fromProvince = watch("fromProvince");
+  const toProvince = watch("toProvince");
+
+  const { data: province } = useFetchProvince();
+  const { data: cities } = useFetchCity({ provinceId: fromProvince });
+  const { data: toCities } = useFetchCity({ provinceId: toProvince });
+  const { mutate, isLoading } = useFecthCost();
+
+  const [result, setResult] = useState<Courier[]>([]);
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    mutate(
+      {
+        origin: data.fromCity,
+        destination: data.toCity,
+        weight: data.weight.toString(),
+        courier: data.courier,
+      },
+      {
+        onSuccess: (data) => {
+          setResult(data.rajaongkir.results);
+        },
+      },
+    );
+  };
+
   return (
     <MainStyled>
       <HeaderStyled>
@@ -24,86 +65,175 @@ function App() {
             menggunakan jasa kurir JNE, POS, dan Tiki
           </Text>
         </Hero>
-        <FilterContainer>
-          <FilterItem>
-            <Text size="lg" weight="bold">
-              Asal
-            </Text>
-            <Item>
-              <SelectFilter>
-                <Text size="sm">Provinsi :</Text>
-                <Select
-                  allowClear
-                  options={[{ label: "Jakarta", value: "jkt" }]}
-                  placeholder="Pilih"
-                />
-              </SelectFilter>
-              <SelectFilter>
-                <Text size="sm">Kota/Kabupaten :</Text>
-                <Select
-                  allowClear
-                  options={[{ label: "Jakarta", value: "jkt" }]}
-                  placeholder="Pilih"
-                />
-              </SelectFilter>
-            </Item>
-          </FilterItem>
-          <FilterItem>
-            <Text size="lg" weight="bold">
-              Tujuan
-            </Text>
-            <Item>
-              <SelectFilter>
-                <Text size="sm">Provinsi :</Text>
-                <Select
-                  allowClear
-                  options={[{ label: "Jakarta", value: "jkt" }]}
-                  placeholder="Pilih"
-                />
-              </SelectFilter>
-              <SelectFilter>
-                <Text size="sm">Kota/Kabupaten :</Text>
-                <Select
-                  allowClear
-                  options={[{ label: "Jakarta", value: "jkt" }]}
-                  placeholder="Pilih"
-                />
-              </SelectFilter>
-            </Item>
-          </FilterItem>
-          <FilterItem>
-            <Text size="lg" weight="bold">
-              Kurir
-            </Text>
-            <Item>
-              <CourierCard
-                className="active"
-                $color={Colors.indicator.green.fg}
-                $bg={Colors.indicator.green.bg}
-              >
-                JNE
-              </CourierCard>
-              <CourierCard
-                $color={Colors.indicator.purple.fg}
-                $bg={Colors.indicator.purple.bg}
-              >
-                POS
-              </CourierCard>
-              <CourierCard
-                $color={Colors.indicator.orange.fg}
-                $bg={Colors.indicator.orange.bg}
-              >
-                TIKI
-              </CourierCard>
-            </Item>
-          </FilterItem>
-          <FilterItem>
-            <Button type="primary">Cek Ongkir</Button>
-            <Button>Simpan Filter</Button>
-          </FilterItem>
-        </FilterContainer>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FilterContainer>
+            <FilterItem>
+              <Text size="lg" weight="bold">
+                Asal
+              </Text>
+              <Item>
+                <SelectFilter>
+                  <Text size="sm">Provinsi :</Text>
+                  <Controller
+                    control={control}
+                    name="fromProvince"
+                    rules={{ required: true }}
+                    render={({ field: { onChange, value } }) => (
+                      <Select
+                        allowClear
+                        value={value}
+                        options={province?.map((item) => ({
+                          label: item.province,
+                          value: item.province_id,
+                        }))}
+                        placeholder="Pilih"
+                        onChange={onChange}
+                      />
+                    )}
+                  />
+                </SelectFilter>
+                <SelectFilter>
+                  <Text size="sm">Kota/Kabupaten :</Text>
+                  <Controller
+                    control={control}
+                    name="fromCity"
+                    rules={{ required: true }}
+                    render={({ field: { onChange, value } }) => (
+                      <Select
+                        allowClear
+                        value={value}
+                        options={cities?.map((item) => ({
+                          label: item.city_name,
+                          value: item.city_id,
+                        }))}
+                        placeholder="Pilih"
+                        onChange={onChange}
+                      />
+                    )}
+                  />
+                </SelectFilter>
+              </Item>
+            </FilterItem>
+            <FilterItem>
+              <Text size="lg" weight="bold">
+                Tujuan
+              </Text>
+              <Item>
+                <SelectFilter>
+                  <Text size="sm">Provinsi :</Text>
+                  <Controller
+                    control={control}
+                    name="toProvince"
+                    rules={{ required: true }}
+                    render={({ field: { onChange, value } }) => (
+                      <Select
+                        allowClear
+                        value={value}
+                        options={province?.map((item) => ({
+                          label: item.province,
+                          value: item.province_id,
+                        }))}
+                        placeholder="Pilih"
+                        onChange={onChange}
+                      />
+                    )}
+                  />
+                </SelectFilter>
+                <SelectFilter>
+                  <Text size="sm">Kota/Kabupaten :</Text>
+                  <Controller
+                    control={control}
+                    name="toCity"
+                    rules={{ required: true }}
+                    render={({ field: { onChange, value } }) => (
+                      <Select
+                        allowClear
+                        value={value}
+                        options={toCities?.map((item) => ({
+                          label: item.city_name,
+                          value: item.city_id,
+                        }))}
+                        placeholder="Pilih"
+                        onChange={onChange}
+                      />
+                    )}
+                  />
+                </SelectFilter>
+              </Item>
+            </FilterItem>
+            <FilterItem>
+              <Text size="lg" weight="bold">
+                Berat
+              </Text>
+              <Item>
+                <SelectFilter>
+                  <Controller
+                    control={control}
+                    name="weight"
+                    rules={{ required: true }}
+                    render={({ field: { onChange, value } }) => (
+                      <InputNumber
+                        suffix="Kg"
+                        style={{ width: "100px" }}
+                        value={value}
+                        min={1}
+                        max={100}
+                        onChange={onChange}
+                      />
+                    )}
+                  />
+                </SelectFilter>
+              </Item>
+            </FilterItem>
+            <FilterItem>
+              <Text size="lg" weight="bold">
+                Kurir
+              </Text>
+              <Controller
+                control={control}
+                name="courier"
+                rules={{ required: true }}
+                render={({ field: { onChange, value } }) => (
+                  <Item>
+                    <CourierCard
+                      $isActive={value === "jne"}
+                      $color={Colors.indicator.green.fg}
+                      $bg={Colors.indicator.green.bg}
+                      onClick={() => onChange("jne")}
+                    >
+                      JNE
+                    </CourierCard>
+                    <CourierCard
+                      $isActive={value === "pos"}
+                      $color={Colors.indicator.purple.fg}
+                      $bg={Colors.indicator.purple.bg}
+                      onClick={() => onChange("pos")}
+                    >
+                      POS
+                    </CourierCard>
+                    <CourierCard
+                      $isActive={value === "tiki"}
+                      $color={Colors.indicator.orange.fg}
+                      $bg={Colors.indicator.orange.bg}
+                      onClick={() => onChange("tiki")}
+                    >
+                      TIKI
+                    </CourierCard>
+                  </Item>
+                )}
+              />
+            </FilterItem>
+            <FilterItem>
+              <Button loading={isLoading} type="primary" htmlType="submit">
+                Cek Ongkir
+              </Button>
+            </FilterItem>
+          </FilterContainer>
+        </form>
 
-        <ResultContainer>
+        {result.length ? (
+          <ResultContainer>
+            {/*
           <DetailFilter>
             <DetailCheck>
               <Text color={Colors.primary.grayLight} size="sm">
@@ -124,55 +254,48 @@ function App() {
               <Text>2 Kg</Text>
             </DetailCheck>
           </DetailFilter>
+            */}
 
-          <DetailCourier>
-            <DetailHeader>
-              <CourierLogo></CourierLogo>
-              <CourierName>
-                <Text type="tag" size="sm">
-                  JNE
-                </Text>
-                <Text weight="bold">Jalur Nugraha Ekakurir (JNE)</Text>
-              </CourierName>
-            </DetailHeader>
-
-            <CourierService>
-              <ServiceItem>
-                <ServiceDetail>
-                  <Text style={{ display: "flex", gap: "5px" }}>
-                    Same Day Service
-                    <Text type="tag" size="xs">
-                      SDS
+            {result.map((item) => (
+              <DetailCourier>
+                <DetailHeader>
+                  <CourierLogo></CourierLogo>
+                  <CourierName>
+                    <Text type="tag" size="sm">
+                      {item.code}
                     </Text>
-                  </Text>
-                  <Text size="xs">9-10 Hari</Text>
-                </ServiceDetail>
-                <ServiceDetail $align="end">
-                  <Text size="xl" weight="bold">
-                    Rp 100.000,00
-                  </Text>
-                </ServiceDetail>
-              </ServiceItem>
+                    <Text weight="bold">{item.name}</Text>
+                  </CourierName>
+                </DetailHeader>
 
-              <ServiceItem>
-                <ServiceDetail>
-                  <Text style={{ display: "flex", gap: "5px" }}>
-                    Same Day Service
-                    <Text type="tag" size="xs">
-                      SDS
-                    </Text>
-                  </Text>
-                  <Text size="xs">9-10 Hari</Text>
-                </ServiceDetail>
-                <ServiceDetail $align="end">
-                  <Text size="xl" weight="bold">
-                    Rp 100.000,00
-                  </Text>
-                </ServiceDetail>
-              </ServiceItem>
-            </CourierService>
-          </DetailCourier>
-        </ResultContainer>
+                <CourierService>
+                  {item.costs.map((cost, i) => (
+                    <ServiceItem>
+                      <ServiceDetail>
+                        <Text style={{ display: "flex", gap: "5px" }}>
+                          {cost.service}
+                          <Text type="tag" size="xs">
+                            {cost.description}
+                          </Text>
+                        </Text>
+                        <Text size="xs">
+                          {cost.cost ? `${cost?.cost[i]?.etd} Hari` : "-"}{" "}
+                        </Text>
+                      </ServiceDetail>
+                      <ServiceDetail $align="end">
+                        <Text size="xl" weight="bold">
+                          {cost.cost
+                            ? formatRupiah(cost.cost[i].value || 0)
+                            : "-"}
+                        </Text>
+                      </ServiceDetail>
+                    </ServiceItem>
+                  ))}
+                </CourierService>
+              </DetailCourier>
+            ))}
+          </ResultContainer>
+        ) : null}
       </Content>
     </MainStyled>
   );
@@ -245,7 +368,11 @@ const SelectFilter = styled.div`
   gap: 5px;
 `;
 
-const CourierCard = styled.button<{ $color: string; $bg: string }>`
+const CourierCard = styled.div<{
+  $color: string;
+  $bg: string;
+  $isActive: boolean;
+}>`
   flex: 1;
   border: none;
   background: ${(props) => props.$bg};
@@ -255,11 +382,7 @@ const CourierCard = styled.button<{ $color: string; $bg: string }>`
   border-radius: 10px;
   text-transform: uppercase;
   transition: 0.3s ease;
-  outline: 1px solid ${(props) => props.$bg};
-
-  &.active {
-    outline: 2px solid ${(props) => props.$color};
-  }
+  outline: 2px solid ${(props) => (props.$isActive ? props.$color : props.$bg)};
 `;
 
 const ResultContainer = styled.div`
@@ -268,6 +391,9 @@ const ResultContainer = styled.div`
   padding: 20px 10px;
   box-shadow: 1px 1px 50px -10px rgba(0, 0, 0, 0.1);
 `;
+
+{
+  /*
 
 const DetailFilter = styled.div`
   display: flex;
@@ -281,6 +407,9 @@ const DetailCheck = styled.div`
   flex-direction: column;
   gap: 5px;
 `;
+
+*/
+}
 
 const DetailCourier = styled.div`
   margin-top: 20px;
---
+++ src/helpers/hooks/index.tsx
@@ -0,0 +1,37 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getCities, getCost, getProvince } from "../api";
+import { APIRes, CostRequest, Courier } from "../types/responseApi";
+import { AxiosError } from "axios";
+
+export const useFetchProvince = () => {
+  return useQuery({
+    refetchOnWindowFocus: false,
+    retry: false,
+    queryKey: ["province"],
+    queryFn: getProvince,
+    select: (data) => {
+      const newData = data.rajaongkir.results;
+      return newData;
+    },
+  });
+};
+
+export const useFetchCity = ({ provinceId }: { provinceId: string }) => {
+  return useQuery({
+    refetchOnWindowFocus: false,
+    queryKey: ["city", provinceId],
+    retry: false,
+    queryFn: () => getCities(provinceId),
+    select: (data) => {
+      const newData = data.rajaongkir.results;
+      return newData;
+    },
+  });
+};
+
+export const useFecthCost = () => {
+  return useMutation<APIRes<Courier[]>, AxiosError<object>, CostRequest>({
+    mutationKey: ["cost"],
+    mutationFn: (vars) => getCost(vars),
+  });
+};
