236fea3fd6026881f6d7d39760e8d6fe10924494
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2024-09-26 16:40:40 +0700

    feat: localstorage

---
+++ src/helpers/utils/localstorage.ts
@@ -0,0 +1,25 @@
+type LocalStorageKey = "SAVE_FILTER";
+
+export const setLocalStorage = <T>(key: LocalStorageKey, value: T) => {
+  try {
+    const serializedValue = JSON.stringify(value);
+    localStorage.setItem(key, serializedValue);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getLocalStorage = <T>(key: LocalStorageKey): T | null => {
+  try {
+    if (typeof window === "undefined") return null;
+    const value = localStorage.getItem(key);
+
+    if (value === null) {
+      return null;
+    }
+
+    return JSON.parse(value) as T;
+  } catch {
+    return null;
+  }
+};
